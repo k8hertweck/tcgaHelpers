ed026a86e3b0aaf93512daccc90568ae98e7cc6d
Kate Hertweck <k8hertweck@gmail.com>
2020-07-27 08:57:06 -0700

    renaming columns

--- R/extractGenes.R
+++ R/extractGenes.R
@@ -4,9 +4,11 @@
 extractGenes <- function(fpkm, meta, genes) {
   # extract all expression data
   fpkmDat <- as.data.frame(assays(fpkm)[[1]])
-  # select only desired genes and pivot table
-  fpkmGene <- as.data.frame(t(filter(fpkmDat, rownames(fpkmDat) %in% genes$ensembl_gene_id)))
-  fpkmGene <- rownames_to_column(fpkmGene, "barcode")
+  # select only desired genes
+  fpkmGene <- as.data.frame(t(filter(fpkmDat,
+                                     rownames(fpkmDat) %in% genes$ensembl_gene_id)))
+  # replace gene column names
+  rename_with(fpkmGene, ~ genes$external_gene_name[match(., genes$ensembl_gene_id)])
   # bind metadata to gene expression data
   fpkmGene <- full_join(meta, fpkmGene, by="barcode")
   return(fpkmGene)
